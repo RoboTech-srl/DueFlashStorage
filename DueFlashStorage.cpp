3e3b7349c1014022e13aad3eaa7626d5e302d4d2
ppescher <p.messina@robotechsrl.com>
2018-02-09 18:15:13 +0100

    Initial support for STM32L4 internal flash, enhanced write with fill value

--- DueFlashStorage.cpp
+++ DueFlashStorage.cpp
@@ -1,122 +1,128 @@
 #include "DueFlashStorage.h"
+#include <strings.h>
 
-DueFlashStorage::DueFlashStorage() {
-  uint32_t retCode;
+// Flash page buffer (aligned to Flash program size)
+static uint64_t page_buffer[FLASH_PAGE_SIZE / sizeof(uint64_t)];
 
-  /* Initialize flash: 6 wait states for flash writing. */
-  retCode = flash_init(FLASH_ACCESS_MODE_128, 6);
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Flash init failed\n");
-  }
-}
-
-byte DueFlashStorage::read(uint32_t address) {
-  return FLASH_START[address];
-}
-byte* DueFlashStorage::readAddress(uint32_t address) {
-  return FLASH_START+address;
-}
-
-boolean DueFlashStorage::write(uint32_t address, byte value) {
-  uint32_t retCode;
-  uint32_t byteLength = 1;  
-  byte *data;
-
-/*
-  retCode = flash_unlock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + byteLength - 1, 0, 0);
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Failed to unlock flash for write\n");
-    return false;
-  }
-*/
-
-  // write data
-  retCode = flash_write((uint32_t)FLASH_START+address, &value, byteLength, 1);
-  //retCode = flash_write((uint32_t)FLASH_START, data, byteLength, 1);
-
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Flash write failed\n");
-    return false;
-  }
-
-/*
-  // Lock page
-  retCode = flash_lock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + byteLength - 1, 0, 0);
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Failed to lock flash page\n");
-    return false;
-  }
-   */
-   
-  return true;
- 
+bool DueFlashStorage::write(uint32_t address, byte *data, uint32_t dataLength)
+{
+  FLASH_EraseInitTypeDef EraseInitStruct;
   
-}
-
-boolean DueFlashStorage::write(uint32_t address, byte *data, uint32_t dataLength) {
-  uint32_t retCode;
-
-  if ((uint32_t)FLASH_START+address < IFLASH1_ADDR) {
-    _FLASH_DEBUG("Flash write address too low\n");
-    return false;
-  }
-
-  if ((uint32_t)FLASH_START+address >= (IFLASH1_ADDR + IFLASH1_SIZE)) {
-    _FLASH_DEBUG("Flash write address too high\n");
-    return false;
-  }
+  if (dataLength == 0)
+    return true;
   
-   _FLASH_DEBUG("FLASH DEBUG\n");
-   _FLASH_DEBUG(IFLASH1_ADDR);
-   _FLASH_DEBUG("\n");
-   _FLASH_DEBUG(IFLASH1_SIZE);
-   _FLASH_DEBUG("\n");
-   
-  // _FLASH_DEBUG(FLASH_START);   
-   _FLASH_DEBUG(address);
-   _FLASH_DEBUG("\n");
+  HAL_FLASH_Unlock();
 
-   _FLASH_DEBUG(dataLength);
-   _FLASH_DEBUG("\n");
-   
-   
+  uint32_t page = (FLASH_STORAGE_START + address - FLASH_BASE) / FLASH_PAGE_SIZE;
+  uint32_t offs = (FLASH_STORAGE_START + address - FLASH_BASE) % FLASH_PAGE_SIZE;
+  uint8_t* padr = (uint8_t*)(FLASH_BASE + page * FLASH_PAGE_SIZE);
+
+  // before the loop, it can happen only the first time
+  if (offs > 0)
+    memcpy(page_buffer, padr, offs);
+  while (dataLength > 0)
+  {
+    // copy a portion of the source buffer that fits in the current page
+    uint32_t size = min(FLASH_PAGE_SIZE - offs,	dataLength);
+    memcpy((uint8_t*)page_buffer + offs, data, size);
+    // any padding at the end?
+    if (offs + size < FLASH_PAGE_SIZE)
+      memcpy((uint8_t*)page_buffer + offs + size, padr + offs + size, FLASH_PAGE_SIZE - (offs + size));
+
+    // erase current page
+    EraseInitStruct.TypeErase   = FLASH_TYPEERASE_PAGES;
+    EraseInitStruct.Banks       = FLASH_BANK_1;
+    EraseInitStruct.Page        = page;
+    EraseInitStruct.NbPages     = 1;
+
+    uint32_t PAGEError = 0;
+    if (HAL_FLASHEx_Erase(&EraseInitStruct, &PAGEError) != HAL_OK)
+    {
+      HAL_FLASH_Lock();
+      return false;
+    }
     
+    // write back orginal data, overwritten by source buffer
+    for (int i = 0; i < FLASH_PAGE_SIZE / sizeof(uint64_t); ++i)
+    {
+      if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, (uint32_t)padr, page_buffer[i]) != HAL_OK)
+      {
+        HAL_FLASH_Lock();
+        return false;
+      }
+      padr += sizeof(uint64_t);
+    }
 
-/*
-  if (((uint32_t)FLASH_START+address & 3) != 0) {
-    _FLASH_DEBUG("Flash start address must be on four byte boundary\n");
-    return false;
-  }
-  
-  */
-  
-
-/*
-  // Unlock page
-  retCode = flash_unlock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + dataLength - 1, 0, 0);
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Failed to unlock flash for write\n");
-    return false;
-  }
-  */
-
-  // write data
-  retCode = flash_write((uint32_t)FLASH_START+address, data, dataLength, 1);
-
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Flash write failed\n");
-    return false;
+		// advance buffer
+		dataLength -= size;
+    if (dataLength == 0)
+      break;
+		data += size;
+		page++;
+		offs = 0;
   }
 
-/*
-  // Lock page
-    retCode = flash_lock((uint32_t)FLASH_START+address, (uint32_t)FLASH_START+address + dataLength - 1, 0, 0);
-  if (retCode != FLASH_RC_OK) {
-    _FLASH_DEBUG("Failed to lock flash page\n");
-    return false;
-  }
-  */
-  
+  HAL_FLASH_Lock();
   return true;
 }
 
+bool DueFlashStorage::write(uint32_t address, byte value, uint32_t dataLength)
+{
+  FLASH_EraseInitTypeDef EraseInitStruct;
+  
+  if (dataLength == 0)
+    return true;
+  
+  HAL_FLASH_Unlock();
+
+  uint32_t page = (FLASH_STORAGE_START + address - FLASH_BASE) / FLASH_PAGE_SIZE;
+  uint32_t offs = (FLASH_STORAGE_START + address - FLASH_BASE) % FLASH_PAGE_SIZE;
+  uint8_t* padr = (uint8_t*)(FLASH_BASE + page * FLASH_PAGE_SIZE);
+
+  // before the loop, it can happen only the first time
+  if (offs > 0)
+    memcpy(page_buffer, padr, offs);
+  while (dataLength > 0)
+  {
+    // fill a portion of the destination that fits in the current page
+    uint32_t size = min(FLASH_PAGE_SIZE - offs,	dataLength);
+    memset((uint8_t*)page_buffer + offs, value, size);
+    // any padding at the end?
+    if (offs + size < FLASH_PAGE_SIZE)
+      memcpy((uint8_t*)page_buffer + offs + size, padr + offs + size, FLASH_PAGE_SIZE - (offs + size));
+
+    // erase current page
+    EraseInitStruct.TypeErase   = FLASH_TYPEERASE_PAGES;
+    EraseInitStruct.Banks       = FLASH_BANK_1;
+    EraseInitStruct.Page        = page;
+    EraseInitStruct.NbPages     = 1;
+
+    uint32_t PAGEError = 0;
+    if (HAL_FLASHEx_Erase(&EraseInitStruct, &PAGEError) != HAL_OK)
+    {
+      HAL_FLASH_Lock();
+      return false;
+    }
+    
+    // write back orginal data, overwritten by source buffer
+    for (int i = 0; i < FLASH_PAGE_SIZE / sizeof(uint64_t); ++i)
+    {
+      if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD, (uint32_t)padr, page_buffer[i]) != HAL_OK)
+      {
+        HAL_FLASH_Lock();
+        return false;
+      }
+      padr += sizeof(uint64_t);
+    }
+
+		// advance buffer
+		dataLength -= size;
+    if (dataLength == 0)
+      break;
+		page++;
+		offs = 0;
+  }
+
+  HAL_FLASH_Lock();
+  return true;
+}
--- DueFlashStorage.h
+++ DueFlashStorage.h
@@ -15,37 +15,23 @@ Rewritten and modified by Sebastian Nilsson
 #define DUEFLASHSTORAGE_H
 
 #include <Arduino.h>
-#include "flash_efc.h"
 
-// 1Kb of data
-#define DATA_LENGTH   ((IFLASH1_PAGE_SIZE/sizeof(byte))*4)
-
-// choose a start address that's offset to show that it doesn't have to be on a page boundary
-#define  FLASH_START  ((byte *)IFLASH1_ADDR)
-
-//  FLASH_DEBUG can be enabled to get debugging information displayed.
-#define FLASH_DEBUG
-
-#ifdef FLASH_DEBUG
-#define _FLASH_DEBUG(x) SerialUSB.print(x);
-#else
-#define _FLASH_DEBUG(x)
-#endif
+#define FLASH_STORAGE_START (FLASH_BASE + 131072u)
+#define FLASH_STORAGE_SIZE  131072u
 
 //  DueFlash is the main class for flash functions
-class DueFlashStorage {
+class DueFlashStorage
+{
 public:
-  DueFlashStorage();
-  
   // write() writes the specified amount of data into flash.
   // flashStart is the address in memory where the write should start
   // data is a pointer to the data to be written
   // dataLength is length of data in bytes
   
-	byte read(uint32_t address);
-	byte* readAddress(uint32_t address);
-	boolean write(uint32_t address, byte value);
-	boolean write(uint32_t address, byte *data, uint32_t dataLength);
+  byte read(uint32_t address) { return *(byte*)(FLASH_STORAGE_START + address); }
+  byte* readAddress(uint32_t address) { return (byte*)(FLASH_STORAGE_START + address); }
+  bool write(uint32_t address, byte *data, uint32_t dataLength);
+  bool write(uint32_t address, byte value, uint32_t dataLength = 1);
 };
 
 #endif
